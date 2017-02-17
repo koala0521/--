eecd22a808eec19df825d6e186df4484cd26640d
ruoShui007 <328762691@qq.com>
2017-02-18 00:16:46 +0800

    个人作品

--- JD-mobile/js/javascript.js
+++
@@ -1,5 +0,0 @@
-			var dpr = 1 / window.devicePixelRatio;
-			document.write('<meta name="viewport" content="width=device-width,user-scalable=no,initial-scale='+dpr+',minimum-scale='+dpr+',maximum-scale='+dpr+'" />')
-			var fz = document.documentElement.clientWidth / 10;
-			//alert(fz);
-			document.getElementsByTagName('html')[0].style.fontSize = fz + 'px';
